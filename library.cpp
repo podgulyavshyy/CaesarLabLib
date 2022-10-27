57c14d61edf079869058d7144b27982671d90bad
podgulyavshyy <mhunko@yahoo.com>
2022-10-27 20:12:29 +0300

    Added library logic

--- library.cpp
+++ library.cpp
@@ -1,7 +1,35 @@
 #include "library.h"
-
 #include <iostream>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+char DecryptChar(char a, int key){
+    a = tolower(a);
+    const std::string albet{"abcdefghijklmnopqrstuvwxyz"};
+    int i = 0;
+    while(a != albet[i]){
+        i++;
+    }
+    key = i - key;
+    while(key < 0){
+        key += 25;
+    }
+    while(key > 25){
+        key -= 25;
+    }
+    return albet[key];
+}
+
+char EncryptChar(char a, int key){
+    const std::string albet{"abcdefghijklmnopqrstuvwxyz"};
+    int i = 0;
+    while(a != albet[i]){
+        i++;
+    }
+    key = key + i;
+    while(key < 0){
+        key += 25;
+    }
+    while(key > 25){
+        key -= 25;
+    }
+    return albet[key];
 }
--- library.h
+++ library.h
@@ -1,6 +1,8 @@
 #ifndef SHAREDCAESAR_LIBRARY_H
 #define SHAREDCAESAR_LIBRARY_H
 
-void hello();
+char Encrypt(char a,int key);
+char Decrypt(char a,int key);
+
 
 #endif //SHAREDCAESAR_LIBRARY_H
