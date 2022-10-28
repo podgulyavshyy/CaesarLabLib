5fa0bd523aced49d8ddf2313886baeb6d9c0260a
Andrew <atrybushnyi@kse.org.ua>
2022-10-28 11:49:31 +0300

    Encr/Decrypt to single char input

--- library.cpp
+++ library.cpp
@@ -1,7 +1,7 @@
 #include "library.h"
 #include <iostream>
 
-char DecryptChar(char a, int key){
+char Decrypt(char a, int key){
     if (std::ispunct(a)){
         return a;
     }
@@ -24,7 +24,7 @@ char DecryptChar(char a, int key){
     return albet[key];
 }
 
-char EncryptChar(char a, int key){
+char Encrypt(char a, int key){
     if (std::ispunct(a)){
         return a;
     }
@@ -45,21 +45,4 @@ char EncryptChar(char a, int key){
         key -= 25;
     }
     return albet[key];
-}
-
-std::string Encrypt(std::string text, int key) {
-    std::string encrypted;
-    for (int i = 0; i < text.length(); i++) {
-        encrypted += EncryptChar(text[i], key);
-    }
-    return encrypted;
-}
-
-
-std::string Decrypt(std::string text, int key) {
-    std::string encrypted;
-    for (int i = 0; i < text.length(); i++) {
-        encrypted += DecryptChar(text[i], key);
-    }
-    return encrypted;
 }
